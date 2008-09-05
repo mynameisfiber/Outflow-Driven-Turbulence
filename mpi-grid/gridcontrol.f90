6a3521f71304704c42d32be9af852f0d58046bce
Michael Gorelick <fiber@Amanda.local>
2008-09-05 16:30:10 -0400

    mpi-grid was updated to actually work with any sized grid and so each node knows what coordinates it serves

--- mpi-grid/gridcontrol.f90
+++ mpi-grid/gridcontrol.f90
@@ -1,8 +1,8 @@
 program gridcontrol
 include "mpif.h"
 
-integer :: maxx,maxy,maxz
-integer, DIMENSION(3) :: dims = (/ 3,3,2 /)
+integer :: n=126, localn=42
+integer, DIMENSION(3) :: dims, offset
 integer :: rank, myid, cartrank=0
 integer :: ierr
 integer, DIMENSION(3) :: coords
@@ -12,18 +12,17 @@ call MPI_INIT(ierr)
 call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
 call MPI_COMM_SIZE(MPI_COMM_WORLD, myid, ierr)
 
+dims = (/ n/localn,n/localn,n/localn /)
 call MPI_CART_CREATE(MPI_COMM_WORLD,3,dims, &
 		(/ .TRUE.,.TRUE.,.TRUE. /),.FALSE.,COMM_CART,ierr)
-
 call MPI_CART_MAP(MPI_COMM_WORLD,3,dims, &
 	     (/ .TRUE.,.TRUE.,.TRUE. /),cartrank,ierr)
 
-
 call MPI_CART_COORDS(COMM_CART,cartrank,3, &
 	        coords,ierr)
+offset = coords * localn
 
-print*,"procrank=",rank,"cartrank=",cartrank
-print*,"    (x,y,z) = ",coords
+print*,"procrank=",rank,"cartrank=",cartrank,"(x,y,z) = ",offset
 
 call MPI_FINALIZE(ierr)
 
